9dac0d0b9f821df1c7a18d5cec3341ffaf93cfe3
Alex M <milesAwayAlex@gmail.com>
2021-07-07 18:59:08 -0700

    Add URL ownership

--- expressServer.js
+++ expressServer.js
@@ -6,8 +6,8 @@ import { nanoid } from 'nanoid';
 const port = 8080;
 const app = express();
 const urlDatabase = {
-  b2xVn2: 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com',
+  b2xVn2: { longURL: 'http://www.lighthouselabs.ca', userID: 'userRandomID' },
+  '9sm5xK': { longURL: 'http://www.google.com', userID: 'user2RandomID' },
 };
 const users = {
   userRandomID: {
@@ -39,21 +39,29 @@ app.get('/urls', (req, res) => {
 });
 app.get('/urls/new', (req, res) => {
   const user = users[req.cookies.user_id];
-  res.render('urlsNew', { user });
+  if (!user) return res.redirect('/login');
+  return res.render('urlsNew', { user });
 });
 app.get('/urls/:shortURL', (req, res) => {
   const { shortURL } = req.params;
   const user = users[req.cookies.user_id];
-  const longURL = urlDatabase[shortURL];
+  const { longURL } = urlDatabase[shortURL];
   res.render('urlShow', { shortURL, longURL, user });
 });
-app.get('/urls.json', (req, res) => res.json(urlDatabase));
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
+
+// app.get('/urls.json', (req, res) => res.json(urlDatabase));
+
+// app.get('/hello', (req, res) => {
+//   res.send('<html><body>Hello <b>World</b></body></html>\n');
+// });
+
 app.get('/u/:shortURL', (req, res) => {
-  const url = urlDatabase[req.params.shortURL] || '/urls/new';
-  res.redirect(url);
+  try {
+    const { longURL } = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+  } catch (err) {
+    res.status(404).send('URL not found');
+  }
 });
 
 app.post('/register', (req, res) => {
@@ -69,10 +77,16 @@ app.post('/register', (req, res) => {
   return res.cookie('user_id', id).redirect('/urls');
 });
 app.post('/urls', (req, res) => {
+  const user = users[req.cookies.user_id];
+  if (!user) {
+    return res
+      .status(401)
+      .send('Only logged-in users can generate new records');
+  }
   const id = nanoid(6);
-  const { longURL } = req.body;
-  urlDatabase[id] = fixHTTP(longURL);
-  res.redirect(`/urls/${id}`);
+  const longURL = fixHTTP(req.body.longURL);
+  urlDatabase[id] = { longURL }; // TODO
+  return res.redirect(`/urls/${id}`);
 });
 app.post('/urls/:id/delete', (req, res) => {
   const { id } = req.params;
@@ -81,7 +95,8 @@ app.post('/urls/:id/delete', (req, res) => {
 });
 app.post('/urls/:id', (req, res) => {
   const { id } = req.params;
-  urlDatabase[id] = fixHTTP(req.body.newURL);
+  const longURL = fixHTTP(req.body.newURL);
+  urlDatabase[id] = { ...urlDatabase[id], longURL };
   res.redirect('/urls');
 });
 app.post('/login', (req, res) => {
