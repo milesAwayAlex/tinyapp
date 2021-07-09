e90b309de4f7c8a06a30ec8c1724955d786c3ece
Alex M <milesAwayAlex@gmail.com>
2021-07-08 23:00:25 -0700

    Comment helper functions

--- helpers.js
+++ helpers.js
@@ -1,9 +1,15 @@
+// prepends protocol to the addresses that don't have it
 const fixHTTP = (address) =>
   (address.startsWith('http://') ? address : `http://${address}`);
+
+// returns the user with given email if found, undefined otherwise
 const findUser = (searchEmail, db) =>
   Object.values(db).find(({ email }) => email === searchEmail);
+
+// returns an object with urls that the user with given id owns
 const urlsForUser = (id, db) =>
   Object.fromEntries(
+    // filter the entries, matching the owner userID
     Object.entries(db).filter(([, { userID }]) => userID === id),
   );
 
